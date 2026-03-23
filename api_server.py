617bd81720c982087ee105a2f858e50c62e3adfe
Joshua Weg <Joshua.weg.95@gmail.com>
2026-03-23 00:23:14 -0700

    updated cors

--- api_server.py
+++ api_server.py
@@ -29,7 +29,7 @@ from captum.attr import IntegratedGradients
 # Initialize Flask app
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024
-CORS(app)  # Enable CORS for Next.js frontend
+CORS(app,origins=['http://analystrix.com'])  # Enable CORS for Next.js frontend
 
 # ---------------------------------------------------------------------------
 # Idle watchdog — stops the EC2 instance after N minutes of no requests
